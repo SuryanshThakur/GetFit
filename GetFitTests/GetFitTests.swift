72b7bae9b0fa22a7af5444f03539735d93edae1f
Suryansh Thakur <thakursuryansh15@gmail.com>
2025-05-14 01:49:08 +0530

    Initial Commit

---
+++ GetFitTests/GetFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  GetFitTests.swift
+//  GetFitTests
+//
+//  Created by Suryansh Thakur on 14/05/25.
+//
+
+import Testing
+@testable import GetFit
+
+struct GetFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
